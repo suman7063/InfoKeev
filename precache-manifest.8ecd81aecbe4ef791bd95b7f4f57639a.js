d47c217449f1e6edbacce3626da6a9bb20bd9e58
suman <singh1994suman2010@gmail.com>
2020-09-28 05:51:06 +0530

    Updates

---
+++ precache-manifest.8ecd81aecbe4ef791bd95b7f4f57639a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ae9ba6d2f7450fa1437802211576f2",
+    "url": "/InfoKeev/index.html"
+  },
+  {
+    "revision": "46390e4dd75e048ff034",
+    "url": "/InfoKeev/static/css/main.346dfceb.chunk.css"
+  },
+  {
+    "revision": "cb842330254696216e2f",
+    "url": "/InfoKeev/static/js/2.ae77ec44.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/InfoKeev/static/js/2.ae77ec44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46390e4dd75e048ff034",
+    "url": "/InfoKeev/static/js/main.a162842a.chunk.js"
+  },
+  {
+    "revision": "49821d59dc84d5021ff9",
+    "url": "/InfoKeev/static/js/runtime-main.e7bf39c0.js"
+  }
+]);
